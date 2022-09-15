d8a1ae1921894b96d0db9b35fe0022ec1c38972b
tr1x <39650214+tr1xxx@users.noreply.github.com>
2022-09-16 00:29:21 +0100

    updated example to print out valid & invalid memory regions

--- battleye-region-walking/battleye-region-walking/main.cpp
+++ battleye-region-walking/battleye-region-walking/main.cpp
@@ -1,6 +1,29 @@
-#include <iostream>
+#include "framework.h"
+#include "region-walker.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	for (const auto& it : BE::SearchMemoryRegions(main))
+	{
+		switch (it.regionType)
+		{
+		    case BE::RegionType::MEMORY_VALID:
+			{
+				std::printf("Valid Memory Region: %p\n", it.regionMBI.BaseAddress);
+				break;
+			}
+			case BE::RegionType::MEMORY_INVALID:
+			{
+				std::printf("Invalid Memory Region: %p\n", it.regionMBI.BaseAddress);
+				break;
+			}
+			default:
+			{
+				std::printf("Unknown Memory RegionType!\n");
+				break;
+			}
+		}
+	}
+
+	return 0;
 }
